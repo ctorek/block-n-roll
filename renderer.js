13fb73036bd797648ea84f5217f5027bee49fc7f
ctorek <ctorek@protonmail.ch>
2023-02-12 20:09:56 -0500

    started blockly toolbox categories

--- renderer.js
+++ renderer.js
@@ -1,18 +1,40 @@
-const blockly = require("blockly");
+const Blockly = require("blockly");
 const pythonGen = require("blockly/python");
 
 // https://developers.google.com/blockly/guides/configure/web/toolbox
 const toolbox = {
-    "kind": "flyoutToolbox",
+    "kind": "categoryToolbox",
     "contents": [
         {
-            "kind": "block",
-            "type": "controls_if"
+            "kind": "category",
+            "name": "Events",
+            "contents": []
+        },
+        {
+            "kind": "category",
+            "name": "Logic",
+            "contents": []
+        },
+        {
+            "kind": "category",
+            "name": "Math",
+            "contents": []
+        },
+        {
+            "kind": "category",
+            "name": "Motors",
+            "contents": []
+        },
+        {
+            "kind": "category",
+            "name": "Sensors",
+            "contents": []
         }
     ]
-};
+}
 
 // inject blockly into document
-const workspace = blockly.inject('blocklyDiv', {
-    toolbox: toolbox
+const workspace = Blockly.inject('blocklyDiv', {
+    toolbox: toolbox,
+    trashcan: true
 });
